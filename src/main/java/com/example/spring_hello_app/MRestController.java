f798114eefc830168c264a53769bce7485347189
Harti Alexandrina <hartialexandrina@gmail.com>
2025-07-22 17:45:33 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_hello_app/MRestController.java
@@ -0,0 +1,26 @@
+package com.example.spring_hello_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+public class MRestController {
+
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World from Spring!";
+    }
+
+    @GetMapping("/students")
+    public List<String> getStudents() {
+        return List.of("Adrian Cojocaru", "Viorel Balan", "Marian Botnarenco");
+    }
+
+    @GetMapping("/info")
+    public String getAppInfo() {
+        return "Aplicatie Spring Boot demo cu endpointuri simple si autentificare de baza.";
+    }
+
+}
