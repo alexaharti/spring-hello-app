83bf02c6ae8bbc7b356da2007f0be12784126b44
Harti Alexandrina <hartialexandrina@gmail.com>
2025-07-22 17:29:47 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_hello_app/SpringHelloAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_hello_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_hello_app/SpringHelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_hello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
